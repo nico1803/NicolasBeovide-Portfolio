626378392aa0a6a98e5c7cf4e2c52c0512844361
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-03-10 05:10:18 -0300

    init portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900">
+      <h1 className="text-3xl font-bold bg-slate-400">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
